b42bcc0670fb46552f324c0c6d9492e8f8775cb8
Zmarfan <zmarfan@hotmail.com>
2021-07-26 22:52:04 +0200

    HTML tags works as intended

--- ProgrammerUtils/HtmlCenter.cs
+++ ProgrammerUtils/HtmlCenter.cs
@@ -13,24 +13,6 @@ namespace ProgrammerUtils
         public static readonly int RAISED_OFFSET = 7;
         public static readonly int LOWERED_OFFSET = -7;
 
-        private static readonly string BOLD_START_TAG = "<b>";
-        private static readonly string BOLD_END_TAG = "</b>";
-
-        private static readonly string ITALICS_START_TAG = "<i>";
-        private static readonly string ITALICS_END_TAG = "</i>";
-
-        private static readonly string UNDER_SCORE_START_TAG = "<u>";
-        private static readonly string UNDER_SCORE_END_TAG = "</u>";
-
-        private static readonly string STRIKE_THROUGH_START_TAG = "<del>";
-        private static readonly string STRIKE_THROUGH_END_TAG = "</del>";
-
-        private static readonly string RAISED_START_TAG = "<sup>";
-        private static readonly string RAISED_END_TAG = "</sup>";
-
-        private static readonly string LOWERED_START_TAG = "<sub>";
-        private static readonly string LOWERED_END_TAG = "</sub>";
-
         private readonly RichTextBox _mainInputTextbox;
         private readonly RichTextBox _mainOutputTextbox;
 
@@ -42,8 +24,7 @@ namespace ProgrammerUtils
 
         public void ConvertTextToHTML()
         {
-            bool bold, italics, underScore, strikeThrough, raised, lowered;
-            bold = italics = underScore = strikeThrough = raised = lowered = false;
+            HtmlService service = new HtmlService();
             StringBuilder finalOutput = new StringBuilder();
 
             for (int i = 0; i < _mainInputTextbox.Text.Length; i++)
@@ -51,45 +32,58 @@ namespace ProgrammerUtils
                 _mainInputTextbox.SelectionStart = i;
                 _mainInputTextbox.SelectionLength = 1;
 
-                SetTagEndings(ref bold, ref italics, ref underScore, ref strikeThrough, ref raised, ref lowered, ref finalOutput);
-                SetTagStarts(ref bold, ref italics, ref underScore, ref strikeThrough, ref raised, ref lowered, ref finalOutput);
+                SetTagEndings(ref service, ref finalOutput);
+                SetTagStarts(ref service, ref finalOutput);
 
                 //Add actual text entered
                 finalOutput.Append(_mainInputTextbox.SelectedText);
             }
 
-            AddFinalEndTags(bold, italics, underScore, strikeThrough, raised, lowered, ref finalOutput);
+            finalOutput.Append(service.CloseAllTags());
 
             _mainOutputTextbox.Text = finalOutput.ToString();
         }
 
-        private void SetTagEndings(ref bool bold, ref bool italics, ref bool underScore, ref bool strikeThrough, ref bool raised, ref bool lowered, ref StringBuilder finalOutput)
+        private void SetTagEndings(ref HtmlService service, ref StringBuilder finalOutput)
         {
-            CheckTextStyleEnding(_mainInputTextbox.SelectionFont.Bold, BOLD_START_TAG, BOLD_END_TAG, ref bold, ref finalOutput);
-            CheckTextStyleEnding(_mainInputTextbox.SelectionFont.Italic, ITALICS_START_TAG, ITALICS_END_TAG, ref italics, ref finalOutput);
-            CheckTextStyleEnding(_mainInputTextbox.SelectionFont.Underline, UNDER_SCORE_START_TAG, UNDER_SCORE_END_TAG, ref underScore, ref finalOutput);
-            CheckTextStyleEnding(_mainInputTextbox.SelectionFont.Strikeout, STRIKE_THROUGH_START_TAG, STRIKE_THROUGH_END_TAG, ref strikeThrough, ref finalOutput);
-            CheckTextStyleEnding(_mainInputTextbox.SelectionCharOffset == RAISED_OFFSET, RAISED_START_TAG, RAISED_END_TAG, ref raised, ref finalOutput);
-            CheckTextStyleEnding(_mainInputTextbox.SelectionCharOffset == LOWERED_OFFSET, LOWERED_START_TAG, LOWERED_END_TAG, ref lowered, ref finalOutput);
+            if (!_mainInputTextbox.SelectionFont.Bold && service.IsTagActive(HtmlService.HtmlStyles.BOLD))
+                finalOutput.Append(service.CloseTag(HtmlService.HtmlStyles.BOLD));
+
+            if (!_mainInputTextbox.SelectionFont.Italic && service.IsTagActive(HtmlService.HtmlStyles.ITALICS))
+                finalOutput.Append(service.CloseTag(HtmlService.HtmlStyles.ITALICS));
+
+            if (!_mainInputTextbox.SelectionFont.Underline && service.IsTagActive(HtmlService.HtmlStyles.UNDERSCORE))
+                finalOutput.Append(service.CloseTag(HtmlService.HtmlStyles.UNDERSCORE));
+
+            if (!_mainInputTextbox.SelectionFont.Strikeout && service.IsTagActive(HtmlService.HtmlStyles.STRIKETHROUGH))
+                finalOutput.Append(service.CloseTag(HtmlService.HtmlStyles.STRIKETHROUGH));
+
+            if (_mainInputTextbox.SelectionCharOffset != RAISED_OFFSET && service.IsTagActive(HtmlService.HtmlStyles.RAISED))
+                finalOutput.Append(service.CloseTag(HtmlService.HtmlStyles.RAISED));
+
+            if (_mainInputTextbox.SelectionCharOffset != LOWERED_OFFSET && service.IsTagActive(HtmlService.HtmlStyles.LOWERED))
+                finalOutput.Append(service.CloseTag(HtmlService.HtmlStyles.LOWERED));
         }
 
-        private void SetTagStarts(ref bool bold, ref bool italics, ref bool underScore, ref bool strikeThrough, ref bool raised, ref bool lowered, ref StringBuilder finalOutput)
+        private void SetTagStarts(ref HtmlService service, ref StringBuilder finalOutput)
         {
-            CheckTextStyleStart(_mainInputTextbox.SelectionFont.Bold, BOLD_START_TAG, BOLD_END_TAG, ref bold, ref finalOutput);
-            CheckTextStyleStart(_mainInputTextbox.SelectionFont.Italic, ITALICS_START_TAG, ITALICS_END_TAG, ref italics, ref finalOutput);
-            CheckTextStyleStart(_mainInputTextbox.SelectionFont.Underline, UNDER_SCORE_START_TAG, UNDER_SCORE_END_TAG, ref underScore, ref finalOutput);
-            CheckTextStyleStart(_mainInputTextbox.SelectionFont.Strikeout, STRIKE_THROUGH_START_TAG, STRIKE_THROUGH_END_TAG, ref strikeThrough, ref finalOutput);
-            CheckTextStyleStart(_mainInputTextbox.SelectionCharOffset == RAISED_OFFSET, RAISED_START_TAG, RAISED_END_TAG, ref raised, ref finalOutput);
-            CheckTextStyleStart(_mainInputTextbox.SelectionCharOffset == LOWERED_OFFSET, LOWERED_START_TAG, LOWERED_END_TAG, ref lowered, ref finalOutput);
-        }
+            if (_mainInputTextbox.SelectionFont.Bold && !service.IsTagActive(HtmlService.HtmlStyles.BOLD))
+                finalOutput.Append(service.OpenTag(HtmlService.HtmlStyles.BOLD));
 
-        private void CheckTextStyleEnding(bool isTextStyle, string startTag, string endTag, ref bool styleActive, ref StringBuilder output)
-        {
-            if (styleActive && !isTextStyle)
-            {
-                styleActive = false;
-                output.Append(endTag);
-            }
+            if (_mainInputTextbox.SelectionFont.Italic && !service.IsTagActive(HtmlService.HtmlStyles.ITALICS))
+                finalOutput.Append(service.OpenTag(HtmlService.HtmlStyles.ITALICS));
+
+            if (_mainInputTextbox.SelectionFont.Underline && !service.IsTagActive(HtmlService.HtmlStyles.UNDERSCORE))
+                finalOutput.Append(service.OpenTag(HtmlService.HtmlStyles.UNDERSCORE));
+
+            if (_mainInputTextbox.SelectionFont.Strikeout && !service.IsTagActive(HtmlService.HtmlStyles.STRIKETHROUGH))
+                finalOutput.Append(service.OpenTag(HtmlService.HtmlStyles.STRIKETHROUGH));
+
+            if (_mainInputTextbox.SelectionCharOffset == RAISED_OFFSET && !service.IsTagActive(HtmlService.HtmlStyles.RAISED))
+                finalOutput.Append(service.OpenTag(HtmlService.HtmlStyles.RAISED));
+
+            if (_mainInputTextbox.SelectionCharOffset == LOWERED_OFFSET && !service.IsTagActive(HtmlService.HtmlStyles.LOWERED))
+                finalOutput.Append(service.OpenTag(HtmlService.HtmlStyles.LOWERED));
         }
 
         private void CheckTextStyleStart(bool isTextStyle, string startTag, string endTag, ref bool styleActive, ref StringBuilder output)
@@ -99,30 +93,6 @@ namespace ProgrammerUtils
                 styleActive = true;
                 output.Append(startTag);
             }
-        }
-
-        private void AddFinalEndTags(
-            bool bold, 
-            bool italics, 
-            bool underScore, 
-            bool strikeThrough, 
-            bool raised, 
-            bool lowered, 
-            ref StringBuilder finalOutput
-            )
-        {
-            if (bold)
-                finalOutput.Append(BOLD_END_TAG);
-            if (italics)
-                finalOutput.Append(ITALICS_END_TAG);
-            if (underScore)
-                finalOutput.Append(UNDER_SCORE_END_TAG);
-            if (strikeThrough)
-                finalOutput.Append(STRIKE_THROUGH_END_TAG);
-            if (raised)
-                finalOutput.Append(RAISED_END_TAG);
-            if (lowered)
-                finalOutput.Append(LOWERED_END_TAG);
         }
     }
 }
---
+++ ProgrammerUtils/HtmlService.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammerUtils
+{
+    public class HtmlService
+    {
+        public enum HtmlStyles
+        {
+            BOLD,
+            ITALICS,
+            UNDERSCORE,
+            STRIKETHROUGH,
+            RAISED,
+            LOWERED
+        }
+
+        class HtmlEntry
+        {
+            public bool Active { get; set; }
+            public string OpenTag { get; private set; }
+            public string CloseTag { get; private set; }
+
+            public HtmlEntry(string openTag, string closeTag)
+            {
+                Active = false;
+                OpenTag = openTag;
+                CloseTag = closeTag;
+            }
+        }
+
+        private static readonly string BOLD_START_TAG = "<b>";
+        private static readonly string BOLD_END_TAG = "</b>";
+
+        private static readonly string ITALICS_START_TAG = "<i>";
+        private static readonly string ITALICS_END_TAG = "</i>";
+
+        private static readonly string UNDER_SCORE_START_TAG = "<u>";
+        private static readonly string UNDER_SCORE_END_TAG = "</u>";
+
+        private static readonly string STRIKE_THROUGH_START_TAG = "<del>";
+        private static readonly string STRIKE_THROUGH_END_TAG = "</del>";
+
+        private static readonly string RAISED_START_TAG = "<sup>";
+        private static readonly string RAISED_END_TAG = "</sup>";
+
+        private static readonly string LOWERED_START_TAG = "<sub>";
+        private static readonly string LOWERED_END_TAG = "</sub>";
+
+        private readonly Dictionary<HtmlStyles, HtmlEntry> _allStyles = new Dictionary<HtmlStyles, HtmlEntry>()
+            {
+                { HtmlStyles.BOLD, new HtmlEntry(BOLD_START_TAG, BOLD_END_TAG) },
+                { HtmlStyles.ITALICS, new HtmlEntry(ITALICS_START_TAG, ITALICS_END_TAG) },
+                { HtmlStyles.UNDERSCORE, new HtmlEntry(UNDER_SCORE_START_TAG, UNDER_SCORE_END_TAG) },
+                { HtmlStyles.STRIKETHROUGH, new HtmlEntry(STRIKE_THROUGH_START_TAG, STRIKE_THROUGH_END_TAG) },
+                { HtmlStyles.RAISED, new HtmlEntry(RAISED_START_TAG, RAISED_END_TAG) },
+                { HtmlStyles.LOWERED, new HtmlEntry(LOWERED_START_TAG, LOWERED_END_TAG) },
+            };
+
+        private Stack<HtmlStyles> _activeTags = new Stack<HtmlStyles>();
+
+        public bool IsTagActive(HtmlStyles style)
+        {
+            return _activeTags.Contains(style);
+        }
+
+        public string OpenTag(HtmlStyles style)
+        {
+            _activeTags.Push(style);
+            _allStyles[style].Active = true;
+
+            return _allStyles[style].OpenTag;
+        }
+
+        public string CloseTag(HtmlStyles style)
+        {
+            StringBuilder closingTags = new StringBuilder();
+
+            while (_activeTags.Contains(style))
+            {
+                HtmlStyles closeStyle = _activeTags.Pop();
+                closingTags.Append(_allStyles[closeStyle].CloseTag);
+                _allStyles[closeStyle].Active = false;
+            }
+
+            return closingTags.ToString();
+        }
+
+        public string CloseAllTags()
+        {
+            StringBuilder closingTags = new StringBuilder();
+            while (_activeTags.Count > 0)
+                closingTags.Append(_allStyles[_activeTags.Pop()].CloseTag);
+
+            return closingTags.ToString();
+        }
+    }
+}
