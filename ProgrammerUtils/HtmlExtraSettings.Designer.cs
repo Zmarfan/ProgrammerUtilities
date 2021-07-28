d7cf74c8b8dd7d7661bdaa67bd48b0b3ed0e9711
Zmarfan <zmarfan@hotmail.com>
2021-07-29 01:01:08 +0200

    Added so you can add custom controller

--- ProgrammerUtils/HtmlExtraSettings.Designer.cs
+++ ProgrammerUtils/HtmlExtraSettings.Designer.cs
@@ -30,78 +30,103 @@
         {
             this.mainTableLayout = new System.Windows.Forms.TableLayoutPanel();
             this.CustomSettingsFlowLayout = new System.Windows.Forms.FlowLayoutPanel();
-            this.htmlCustomRule1 = new ProgrammerUtils.HtmlCustomRule();
-            this.htmlCustomRule2 = new ProgrammerUtils.HtmlCustomRule();
-            this.htmlCustomRule3 = new ProgrammerUtils.HtmlCustomRule();
+            this.OptionsTableLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
             this.mainTableLayout.SuspendLayout();
-            this.CustomSettingsFlowLayout.SuspendLayout();
+            this.OptionsTableLayout.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainTableLayout
             // 
             this.mainTableLayout.ColumnCount = 2;
-            this.mainTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.mainTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.mainTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.mainTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 400F));
             this.mainTableLayout.Controls.Add(this.CustomSettingsFlowLayout, 1, 0);
+            this.mainTableLayout.Controls.Add(this.OptionsTableLayout, 0, 0);
             this.mainTableLayout.Dock = System.Windows.Forms.DockStyle.Fill;
             this.mainTableLayout.Location = new System.Drawing.Point(0, 0);
             this.mainTableLayout.Name = "mainTableLayout";
             this.mainTableLayout.RowCount = 2;
             this.mainTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.mainTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.mainTableLayout.Size = new System.Drawing.Size(836, 450);
+            this.mainTableLayout.Size = new System.Drawing.Size(527, 309);
             this.mainTableLayout.TabIndex = 0;
             // 
             // CustomSettingsFlowLayout
             // 
             this.CustomSettingsFlowLayout.AutoScroll = true;
-            this.CustomSettingsFlowLayout.Controls.Add(this.htmlCustomRule1);
-            this.CustomSettingsFlowLayout.Controls.Add(this.htmlCustomRule2);
-            this.CustomSettingsFlowLayout.Controls.Add(this.htmlCustomRule3);
             this.CustomSettingsFlowLayout.Dock = System.Windows.Forms.DockStyle.Fill;
             this.CustomSettingsFlowLayout.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.CustomSettingsFlowLayout.Location = new System.Drawing.Point(421, 3);
+            this.CustomSettingsFlowLayout.Location = new System.Drawing.Point(103, 3);
             this.CustomSettingsFlowLayout.Name = "CustomSettingsFlowLayout";
             this.mainTableLayout.SetRowSpan(this.CustomSettingsFlowLayout, 2);
-            this.CustomSettingsFlowLayout.Size = new System.Drawing.Size(412, 444);
+            this.CustomSettingsFlowLayout.Size = new System.Drawing.Size(421, 303);
             this.CustomSettingsFlowLayout.TabIndex = 0;
             this.CustomSettingsFlowLayout.WrapContents = false;
             // 
-            // htmlCustomRule1
+            // OptionsTableLayout
             // 
-            this.htmlCustomRule1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.htmlCustomRule1.Location = new System.Drawing.Point(3, 3);
-            this.htmlCustomRule1.Name = "htmlCustomRule1";
-            this.htmlCustomRule1.Size = new System.Drawing.Size(386, 51);
-            this.htmlCustomRule1.TabIndex = 0;
+            this.OptionsTableLayout.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.OptionsTableLayout.ColumnCount = 1;
+            this.OptionsTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.OptionsTableLayout.Controls.Add(this.SaveButton, 0, 1);
+            this.OptionsTableLayout.Controls.Add(this.AddButton, 0, 0);
+            this.OptionsTableLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.OptionsTableLayout.Location = new System.Drawing.Point(0, 0);
+            this.OptionsTableLayout.Margin = new System.Windows.Forms.Padding(0);
+            this.OptionsTableLayout.Name = "OptionsTableLayout";
+            this.OptionsTableLayout.Padding = new System.Windows.Forms.Padding(5);
+            this.OptionsTableLayout.RowCount = 3;
+            this.mainTableLayout.SetRowSpan(this.OptionsTableLayout, 2);
+            this.OptionsTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.OptionsTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.OptionsTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.OptionsTableLayout.Size = new System.Drawing.Size(100, 309);
+            this.OptionsTableLayout.TabIndex = 1;
             // 
-            // htmlCustomRule2
+            // AddButton
             // 
-            this.htmlCustomRule2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.htmlCustomRule2.Location = new System.Drawing.Point(3, 60);
-            this.htmlCustomRule2.Name = "htmlCustomRule2";
-            this.htmlCustomRule2.Size = new System.Drawing.Size(386, 51);
-            this.htmlCustomRule2.TabIndex = 1;
+            this.AddButton.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.AddButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AddButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AddButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddButton.Location = new System.Drawing.Point(8, 8);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(84, 44);
+            this.AddButton.TabIndex = 0;
+            this.AddButton.Text = "Add Rule";
+            this.AddButton.UseVisualStyleBackColor = false;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
             // 
-            // htmlCustomRule3
+            // SaveButton
             // 
-            this.htmlCustomRule3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.htmlCustomRule3.Location = new System.Drawing.Point(3, 117);
-            this.htmlCustomRule3.Name = "htmlCustomRule3";
-            this.htmlCustomRule3.Size = new System.Drawing.Size(386, 51);
-            this.htmlCustomRule3.TabIndex = 2;
+            this.SaveButton.BackColor = System.Drawing.Color.PaleGreen;
+            this.SaveButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SaveButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SaveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SaveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SaveButton.Location = new System.Drawing.Point(8, 58);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(84, 44);
+            this.SaveButton.TabIndex = 1;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = false;
             // 
             // HtmlExtraSettings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(836, 450);
+            this.ClientSize = new System.Drawing.Size(527, 309);
             this.Controls.Add(this.mainTableLayout);
+            this.MaximumSize = new System.Drawing.Size(545, 2000);
+            this.MinimumSize = new System.Drawing.Size(545, 356);
             this.Name = "HtmlExtraSettings";
             this.Text = "Html:ify Custom Settings";
             this.TopMost = true;
             this.mainTableLayout.ResumeLayout(false);
-            this.CustomSettingsFlowLayout.ResumeLayout(false);
+            this.OptionsTableLayout.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -110,8 +135,8 @@
 
         private System.Windows.Forms.TableLayoutPanel mainTableLayout;
         private System.Windows.Forms.FlowLayoutPanel CustomSettingsFlowLayout;
-        private HtmlCustomRule htmlCustomRule1;
-        private HtmlCustomRule htmlCustomRule2;
-        private HtmlCustomRule htmlCustomRule3;
+        private System.Windows.Forms.TableLayoutPanel OptionsTableLayout;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button SaveButton;
     }
 }
--- ProgrammerUtils/HtmlExtraSettings.cs
+++ ProgrammerUtils/HtmlExtraSettings.cs
@@ -12,9 +12,30 @@ namespace ProgrammerUtils
 {
     public partial class HtmlExtraSettings : Form
     {
+        private int _indexGiver = int.MinValue;
+        private Dictionary<int, HtmlCustomRule> _allCustomRules = new Dictionary<int, HtmlCustomRule>();
+
         public HtmlExtraSettings()
         {
             InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            while (_allCustomRules.ContainsKey(_indexGiver))
+                _indexGiver++;
+
+            HtmlCustomRule newRule = new HtmlCustomRule() { Id = _indexGiver };
+            newRule.OnDeleteButtonPressed += DeleteButtonPressed;
+
+            CustomSettingsFlowLayout.Controls.Add(newRule);
+            _allCustomRules.Add(newRule.Id, newRule);
+        }
+
+        private void DeleteButtonPressed(int id)
+        {
+            CustomSettingsFlowLayout.Controls.Remove(_allCustomRules[id]);
+            _allCustomRules.Remove(id);
         }
     }
 }
